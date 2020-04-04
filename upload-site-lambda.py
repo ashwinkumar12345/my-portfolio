c05f0a3ebba1a98674490fa8aa31cfb7a7701964
ashwinkumar12345 <kumarjao@amazon.com>
2020-04-04 02:20:29 -0700

    added site deployment function in Lambda

--- upload-site-lambda.py
+++ upload-site-lambda.py
@@ -4,17 +4,22 @@ import StringIO
 import zipfile
 import mimetypes
 
-s3 = boto3.resource('s3', config=Config(signature_version='s3v4'))
+def lambda_handler(event, context):
 
-site_bucket = s3.Bucket('www.aitopy.com')
-build_bucket = s3.Bucket('build.aitopy.com')
 
-site_zip = StringIO.StringIO()
-build_bucket.download_fileobj('aitopybuild.zip', site_zip)
+    s3 = boto3.resource('s3', config=Config(signature_version='s3v4'))
 
-with zipfile.ZipFile(site_zip) as myzip:
-    for nm in myzip.namelist():
-        obj = myzip.open(nm)
-        site_bucket.upload_fileobj(obj, nm,
-          ExtraArgs={'ContentType': mimetypes.guess_type(nm)[0]})
-        site_bucket.Object(nm).Acl().put(ACL='public-read')
+    site_bucket = s3.Bucket('www.aitopy.com')
+    build_bucket = s3.Bucket('build.aitopy.com')
+
+    site_zip = StringIO.StringIO()
+    build_bucket.download_fileobj('aitopybuild.zip', site_zip)
+
+    with zipfile.ZipFile(site_zip) as myzip:
+        for nm in myzip.namelist():
+            obj = myzip.open(nm)
+            site_bucket.upload_fileobj(obj, nm,
+              ExtraArgs={'ContentType': mimetypes.guess_type(nm)[0]})
+            site_bucket.Object(nm).Acl().put(ACL='public-read')
+
+    return 'Hello from Lambda!'
