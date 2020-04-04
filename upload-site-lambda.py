308e180e1d05905f8771916bbff5918eefddea6b
ashwinkumar12345 <kumarjao@amazon.com>
2020-04-04 04:36:35 -0700

    Use SNS to notify when site is deployed

--- upload-site-lambda.py
+++ upload-site-lambda.py
@@ -6,20 +6,26 @@ import mimetypes
 
 def lambda_handler(event, context):
 
+    sns = boto3.resource('sns')
+    topic = sns.Topic('arn:aws:sns:us-east-1:726040156820:deploySiteTopic')
 
-    s3 = boto3.resource('s3', config=Config(signature_version='s3v4'))
+    try:
+        s3 = boto3.resource('s3', config=Config(signature_version='s3v4'))
 
-    site_bucket = s3.Bucket('www.aitopy.com')
-    build_bucket = s3.Bucket('build.aitopy.com')
+        site_bucket = s3.Bucket('www.aitopy.com')
+        build_bucket = s3.Bucket('build.aitopy.com')
 
-    site_zip = StringIO.StringIO()
-    build_bucket.download_fileobj('aitopybuild.zip', site_zip)
+        site_zip = StringIO.StringIO()
+        build_bucket.download_fileobj('aitopybuild.zip', site_zip)
+        
+        with zipfile.ZipFile(site_zip) as myzip:
+            for nm in myzip.namelist():
+                obj = myzip.open(nm)
+                site_bucket.upload_fileobj(obj, nm)
+                site_bucket.Object(nm).Acl().put(ACL='public-read')
 
-    with zipfile.ZipFile(site_zip) as myzip:
-        for nm in myzip.namelist():
-            obj = myzip.open(nm)
-            site_bucket.upload_fileobj(obj, nm,
-              ExtraArgs={'ContentType': mimetypes.guess_type(nm)[0]})
-            site_bucket.Object(nm).Acl().put(ACL='public-read')
-
-    return 'Hello from Lambda!'
+        print 'Job Done!'
+        topic.publish(Subject="Site Deployed", Message="Site deployed successfully!")
+    except:
+        topic.publish(Subject="Site Deploy Failed", Message="Site was NOT deployed successfully!")
+        raise
