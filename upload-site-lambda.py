166d6f5edfe5c1d0342cd5cd5586640344805f2d
ashwinkumar12345 <kumarjao@amazon.com>
2020-04-04 15:07:13 -0700

    Use bucket name and object key from codepipeline, and notify when codepipeline job done

--- upload-site-lambda.py
+++ upload-site-lambda.py
@@ -9,23 +9,44 @@ def lambda_handler(event, context):
     sns = boto3.resource('sns')
     topic = sns.Topic('arn:aws:sns:us-east-1:726040156820:deploySiteTopic')
 
+    location = {
+        "bucketName": 'build.aitopy.com',
+        "objectKey": 'aitopybuild.zip'
+
+    }
+
     try:
+        job = event.get('CodePipeline.job')
+
+        if job:
+            for artifact in job["data"]["inputArtifacts"]:
+                if artifact["name"] == "BuildArtifact":
+                    location = artifact["location"]["s3Location"]
+
+        print ("Building site from " + str(location))
+
         s3 = boto3.resource('s3', config=Config(signature_version='s3v4'))
 
         site_bucket = s3.Bucket('www.aitopy.com')
-        build_bucket = s3.Bucket('build.aitopy.com')
+        build_bucket = s3.Bucket(location["bucketName"])
 
         site_zip = StringIO.StringIO()
-        build_bucket.download_fileobj('aitopybuild.zip', site_zip)
-        
+        build_bucket.download_fileobj(location["objectKey"], site_zip)
+
         with zipfile.ZipFile(site_zip) as myzip:
             for nm in myzip.namelist():
                 obj = myzip.open(nm)
-                site_bucket.upload_fileobj(obj, nm)
+                site_bucket.upload_fileobj(obj, nm, ExtraArgs={'ContentType': mimetypes.guess_type(nm)[0]})
                 site_bucket.Object(nm).Acl().put(ACL='public-read')
 
         print 'Job Done!'
         topic.publish(Subject="Site Deployed", Message="Site deployed successfully!")
+
+        if job:
+            codepipeline = boto3.client('codepipeline')
+            codepipeline.put_job_success_result(jobId = job["id"])
+
     except:
         topic.publish(Subject="Site Deploy Failed", Message="Site was NOT deployed successfully!")
         raise
+    return 'Hello from Lambda'
