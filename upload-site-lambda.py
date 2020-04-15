4807172c92b2308ae72544bb01192625c1a618f2
ashwinkumar12345 <kumarjao@amazon.com>
2020-04-15 02:04:01 -0700

    Added basic upload script

---
+++ upload-site-lambda.py
@@ -0,0 +1,21 @@
+import boto3
+from botocore.client import Config
+import StringIO
+import zipfile
+import mimetypes
+
+s3 = boto3.resource('s3', config=Config(signature_version='s3v4'))
+
+site_bucket = s3.Bucket('www.aitopy.com')
+build_bucket = s3.Bucket('build.aitopy.com')
+
+site_zip = StringIO.StringIO()
+build_bucket.download_fileobj('aitopybuild.zip', site_zip)
+
+with zipfile.ZipFile(site_zip) as myzip:
+    for nm in myzip.namelist():
+        obj = myzip.open(nm)
+        mime_type = mimetypes.guess_type(nm)[0]
+        site_bucket.upload_fileobj(obj, nm,
+         ExtraArgs={'ContentType': str(mime_type)})
+        site_bucket.Object(nm).Acl().put(ACL='public-read')
